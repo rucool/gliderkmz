14a75804a640371fc6b9da2bf7a09074e0487bac
Lori Garzio <lgarzio@marine.rutgers.edu>
2024-03-15 13:09:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# gliderkmz
+Tools for generating .kmzs for visualizing glider tracks
+
+Authors: Lori Garzio and Laura Nazzaro
+
+## Note: this is under development
+
+## Installation
+
+`git clone https://github.com/rucool/gliderkmz.git`
+
+`cd gliderkmz`
+
+`conda env create -f environment.yml`
+
+`conda activate gliderkmz`
+
+`pip install .`
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gliderkmz',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/rucool/gliderkmz',
+    author='Lori Garzio, Laura Nazzaro',
+    author_email='lgarzio@marine.rutgers.edu, nazzaro@marine.rutgers.edu',
+    description='Tools for generating glider track kmzs'
+)
