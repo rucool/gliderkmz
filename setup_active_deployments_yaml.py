7f47feb9fd1c9f45e7b2591fea926ac1fe0fd7aa
Lori Garzio <lgarzio@marine.rutgers.edu>
2024-05-22 11:26:23 -0400

    Update setup_active_deployments_yaml.py
    - provide directory instead of the name of the file

--- setup_active_deployments_yaml.py
+++ setup_active_deployments_yaml.py
@@ -19,7 +19,7 @@ pd.set_option('display.width', 320, "display.max_columns", 10)
 
 
 def main(args):
-    savefile = args.savefile
+    configdir = args.configdir
 
     # inspired by colorblind-friendly colormap (https://mpetroff.net/2018/03/color-cycle-picker/) for tracks/points
     # NOTE: kml colors are encoded backwards from the HTML convention. HTML colors are "#rrggbbaa": Red Green Blue
@@ -37,6 +37,8 @@ def main(args):
     glider_deployments = []
     for ad in active_deployments_api:
         glider_deployments.append(ad['deployment_name'])
+
+    savefile = os.path.join(configdir, 'active_deployments.yml')
 
     # set up the config file for the first time
     if not os.path.isfile(savefile):
@@ -96,9 +98,10 @@ if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=main.__doc__,
                                          formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    arg_parser.add_argument('-s', '--savefile',
+    arg_parser.add_argument('-c', '--configdir',
                             type=str,
-                            help='Active deployments yaml file')
+                            help='Directory containing active_deployments.yml configuration file (if available), or '
+                                 'directory where new active_deployments.yml file will be saved')
 
     parsed_args = arg_parser.parse_args()
 
